d203e4b7cfe58f96ca745d9433cffa5d8ccb50b7
nicollycostaa <92127366+nicollycostaa@users.noreply.github.com>
2022-04-13 15:04:41 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,18 @@
+const aluno01 ={
+    nome: "nicolly",
+    nota : 9.8
+}
+
+const aluno02 ={
+    nome: "Gabrela",
+    nota: 10
+}
+
+const aluno03 ={
+    nome: "Ana",
+    nota:8.5
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3;
+
+console.log(media);
